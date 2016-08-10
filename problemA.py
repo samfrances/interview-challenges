36bb8255deb45e67a9833c77c85c158f1ecee9e7
Sam Frances <sam@samfrances.uk>
2016-08-10 21:12:56 +0100

    Add module docstrings

--- problemA.py
+++ problemA.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""problemA.py: prints
+the numbers from 1 to 100, one per line.  However for multiples of 3
+prints “Kit” instead of the number and for the multiples of five print
+“Kat”.  For numbers which are a multiple of both 3 and 5, prints “KitKat”.
+"""
 
 # Version 1 of kitkat, where use of the % operator is permitted
 
@@ -8,7 +15,8 @@ def kitkat():
     yields 'Kit' instead of the number and for the multiples of five yields
     'Kat'.  For numbers which are a multiple of both 3 and 5, yields 'KitKat'.
 
-    This version uses the modulo operator to test for divisibility."""
+    This version uses the modulo operator to test for divisibility.
+    """
 
     for n in range(1, 101):
         kit = 'Kit' if n % 3 == 0 else ''
--- problemB.py
+++ problemB.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-# Version 2 of kitkat, where use of the % / or // operators is not permitted
+"""problemB.py: prints
+the numbers from 1 to 100, one per line.  However for multiples of 3
+prints “Kit” instead of the number and for the multiples of five print
+“Kat”.  For numbers which are a multiple of both 3 and 5, prints “KitKat”.
+
+This version does not use any of the division or
+modulus operators ("/", "//", or "%"), either directly or
+indirectly.
+"""
 
 def divisible(dividend, divisor):
     """Determines whether dividend is divisible by divisor.
@@ -23,7 +32,8 @@ def kitkat():
     yields 'Kit' instead of the number and for the multiples of five yields
     'Kat'.  For numbers which are a multiple of both 3 and 5, yields 'KitKat'.
 
-    This version does not use the modulo operator or the division operators."""
+    This version does not use the modulo operator or the division operators.
+    """
 
     for n in range(1, 101):
         kit = 'Kit' if divisible(n, 3) else ''
