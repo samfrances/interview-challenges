60d1528d02e008f7686b39423097950592b57cef
Sam Frances <sam@samfrances.uk>
2016-08-10 21:01:37 +0100

    Revise docstrings

--- problemA.py
+++ problemA.py
@@ -3,11 +3,12 @@
 # Version 1 of kitkat, where use of the % operator is permitted
 
 def kitkat():
-    """Yields the numbers from 1 to 100, one per line.  However for multiples of 3
-    print 'Kit' instead of the number and for the multiples of five print
-    'Kat'.  For numbers which are a multiple of both 3 and 5, print 'KitKat'.
+    """Yields strings representing the integers from 1 to 100 in decimal. 
+    However for multiples of 3
+    yields 'Kit' instead of the number and for the multiples of five yields
+    'Kat'.  For numbers which are a multiple of both 3 and 5, yields 'KitKat'.
 
-    This version uses the modulo operator."""
+    This version uses the modulo operator to test for divisibility."""
 
     for n in range(1, 101):
         kit = 'Kit' if n % 3 == 0 else ''
--- problemB.py
+++ problemB.py
@@ -2,18 +2,26 @@
 
 # Version 2 of kitkat, where use of the % / or // operators is not permitted
 
-def divisible(n, by):
-    """Assumes n and by are positive integers.
-    Returns True if n is divisible by by, else False"""
+def divisible(dividend, divisor):
+    """Determines whether dividend is divisible by divisor.
+
+    Args:
+        dividend (int): the dividend
+        divisor: (int): the divisor
+    Returns:
+        True if divisor is divisible by divisor.
+        False otherwise.
+    """
 
     while n > 0:
         n -= by
     return n == 0
 
 def kitkat():
-    """Yields the numbers from 1 to 100, one per line.  However for multiples of 3
-    print 'Kit' instead of the number and for the multiples of five print
-    'Kat'.  For numbers which are a multiple of both 3 and 5, print 'KitKat'.
+    """Yields strings representing the integers from 1 to 100 in decimal. 
+    However for multiples of 3
+    yields 'Kit' instead of the number and for the multiples of five yields
+    'Kat'.  For numbers which are a multiple of both 3 and 5, yields 'KitKat'.
 
     This version does not use the modulo operator or the division operators."""
 
