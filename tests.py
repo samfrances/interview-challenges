90006815b5818101e9c46e622be9b0c5f12b677b
Sam Frances <sam@samfrances.uk>
2016-08-10 18:45:18 +0100

    Refine tests

--- tests.py
+++ tests.py
@@ -2,26 +2,33 @@ import unittest
 from problemA import kitkat as kitkatA
 from problemB import kitkat as kitkatB
 
+label = {
+	kitkatA: 'A',
+	kitkatB: 'B'
+}
+
 threes = [x * 3 for x in range(1, 35)]
 fives = [x * 5 for x in range(1, 21)]
+
+print(threes)
+print(fives)
+
+kitkat_test_sequence = ['KitKat' if x in threes and x in fives
+						else 'Kit' if x in threes
+						else 'Kat' if x in fives
+						else x for x in range(1, 101)]
 
 class TestKitKats(unittest.TestCase):
 
 	def test_same(self):
-		"""Test that both versions of kitkat return the same result"""
+		"""Sanity check: Test that both versions of kitkat return the same result"""
 		self.assertEqual(list(kitkatA()), list(kitkatB()))
 
 	def test_correct(self):
-		for kitkat in (kitkatA(), kitkatB()):
-			for i, el in enumerate(kitkat):
-				if i+1 in threes and i+1 in fives:
-					self.assertEqual(el, 'KitKat')
-				elif i+1 in threes:
-					self.assertEqual(el, 'Kit')
-				elif i+1 in fives:
-					self.assertEqual(el, 'Kat')
-				else:
-					self.assertEqual(el, i+1)
+		"""Test that both versions of kitkat return the correct result"""
+		for label, kitkat in ('A', kitkatA), ('B', kitkatB):
+			with self.subTest(version=label):
+				self.assertEqual(list(kitkat()), kitkat_test_sequence)
 
 if __name__ == '__main__':
 	unittest.main()
